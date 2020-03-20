9003cb569d6f759621b4fd25fa5fa40954961736
Isaias Soares <isaiassoares@ucl.br>
2020-03-19 23:11:19 -0300

    Primeiro Commit

---
+++ S1.CAP3_Prova_Pratica_Logica_Programacao/S1.CAP3_Prova_Pratica_Logica_Programacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S1.CAP3_Prova_Pratica_Logica_Programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
